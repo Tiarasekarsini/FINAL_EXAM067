c526e312616abdcaabcc1c6ba9a0e04426d3d7b0
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-01-13 14:21:11 +0700

    Menambahkan Method POST pada Controller

--- src/main/java/ws/a/FINAL_EXAM/suratController.java
+++ src/main/java/ws/a/FINAL_EXAM/suratController.java
@@ -5,10 +5,14 @@
  */
 package ws.a.FINAL_EXAM;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpEntity;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,6 +35,26 @@ public class suratController {
        List<Surat> sr = new ArrayList<>(); //declare new list Variable
        sr = jpa.findSuratEntities();//menginisialisasikan isi sr menjadi variable untuk menampilkan semua data
        return sr;
+    }
+    
+    //Menentukan Url permintaan untuk mengakses REST endpoint pada method POST
+    @PostMapping(value = "/POST", consumes = APPLICATION_JSON_VALUE)
+    //method untuk menambahkan data
+    public String tambahData(HttpEntity<String> datasend) throws JsonProcessingException, Exception {
+
+        //membuat variabel sebagai keluaran ketika data tidak berhasil disimpan
+        String Respon = "Tidak Ada Perubahan";
+
+        ObjectMapper maper = new ObjectMapper();
+        //readValue()untuk menerima bentuk input lain
+        surat = maper.readValue(datasend.getBody(), Surat.class);
+        try {
+            jpa.create(surat);
+            Respon = surat.getNoSurat() + " Berhasil Disimpan";
+        } catch (Exception error) {
+            Respon = error.getMessage();
+        }
+        return Respon;
     }
     
 }
