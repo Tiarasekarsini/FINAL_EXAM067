37e62dc9b878b976925271a6b99da93043f7f26c
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-01-13 14:45:37 +0700

    Menambahkan Method PUT dan DELETE pada Controller

--- src/main/java/ws/a/FINAL_EXAM/suratController.java
+++ src/main/java/ws/a/FINAL_EXAM/suratController.java
@@ -11,8 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpEntity;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -57,4 +59,43 @@ public class suratController {
         return Respon;
     }
     
+    @PutMapping(value = "/PUT", consumes = APPLICATION_JSON_VALUE)
+    public String ubahData(HttpEntity<String> datasend) throws JsonProcessingException, Exception {
+        //membuat variabel sebagai keluaran ketika data tidak berhasil disimpan
+        String Respon = "Tidak Ada Perubahan";
+
+        ObjectMapper maper = new ObjectMapper();
+        //readValue()untuk menerima bentuk input lain
+        surat = maper.readValue(datasend.getBody(), Surat.class);
+        try {
+            jpa.edit(surat);
+            Respon = surat.getNoSurat()+ " Berhasil Diedit";
+        } catch (Exception error) {
+            Respon = error.getMessage();
+        }
+        return Respon;
+    }
+    //Menentukan Url permintaan untuk mengakses REST endpoint pada method DELETE
+    @DeleteMapping(value = "/DELETE", consumes = APPLICATION_JSON_VALUE)
+    //method untuk menghapus data 
+    public String delData(HttpEntity<String> datasend) throws JsonProcessingException, Exception {
+        //membuat variabel sebagai keluaran ketika data tidak berhasil disimpan
+        String feedback = "Tidak Ada Perubahan";
+
+        ObjectMapper maper = new ObjectMapper();
+        //readValue()untuk menerima bentuk input lain
+        surat = maper.readValue(datasend.getBody(), Surat.class);
+        try {
+            jpa.destroy(surat.getId()); //delete data berdasarkan id 
+            //menampilkan keluaran ketika data berhasil di deleted
+            feedback = "Berhasil di-Deleted";
+        } catch (Exception error) {
+            //menampilkan keluaran pesan error jika data tidak berhasil ditambahkan
+            feedback = error.getMessage();
+        }
+
+        //menampilkan keluaran dari variabel feedback (tergantung berhasil atau eror)
+        return feedback;
+
+    }
 }
