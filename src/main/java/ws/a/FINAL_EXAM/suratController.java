afcff47edaf24c39dd7147bf9a7e6b027690bee2
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-01-13 14:06:07 +0700

    Menambahkan Method GET pada Controller

--- src/main/java/ws/a/FINAL_EXAM/suratController.java
+++ src/main/java/ws/a/FINAL_EXAM/suratController.java
@@ -5,10 +5,32 @@
  */
 package ws.a.FINAL_EXAM;
 
+import java.util.ArrayList;
+import java.util.List;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author ASUS
  */
+
+@RestController
 public class suratController {
+    
+    //menginisialisasi kelas Surat
+    Surat surat = new Surat();
+    //menginisialisasi Jpa Controller
+    SuratJpaController jpa = new SuratJpaController();
+    
+    //menentukan url permintaan untuk mengakses REST endpoint pada method GET
+    @GetMapping(value = "/GET", produces = APPLICATION_JSON_VALUE)
+    //membuat method untuk menampilkan surat
+    public List<Surat> showSurat(){
+       List<Surat> sr = new ArrayList<>(); //declare new list Variable
+       sr = jpa.findSuratEntities();//menginisialisasikan isi sr menjadi variable untuk menampilkan semua data
+       return sr;
+    }
     
 }
